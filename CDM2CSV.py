c91dbd3e04f7d6323f3865b7a8d8e91386f5f4e7
Jacob Grubb <jgrubb013@gmail.com>
2018-09-12 12:18:49 -0500

    Update CDM2CSV.py
    Put the "getCollectionList" into its own function

--- CDM2CSV.py
+++ CDM2CSV.py
@@ -5,11 +5,15 @@ import sys
 if len(sys.argv)<4:
 	print("Usage: python3 CDM2CSV.py [collections.carli......exe] [username] [password]")
 	sys.exit(1)
-pattern='value="\/.{1,6}_.{1,16}"'
-r=requests.get(sys.argv[1], auth=(sys.argv[2], sys.argv[3]));
-text=re.findall(pattern,r.text)
-collection=[]
-for node in text:
-	collection.append(node[7:-1])
+collectionList = getCollectionList(sys.argv)
 
-print(collection)
+
+
+def getCollectionList(sys.argv):
+	pattern='value="\/.{1,6}_.{1,16}"'
+	r=requests.get(sys.argv[1], auth=(sys.argv[2], sys.argv[3]));
+	text=re.findall(pattern,r.text)
+	collection=[]
+	for node in text:
+		collection.append(node[7:-1])
+	return collection
