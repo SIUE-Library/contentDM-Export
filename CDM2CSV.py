dbfe5649b8eba475b0b700e1b63c5d500bbb97c2
dale auten <dauten96@gmail.com>
2018-09-13 14:24:06 -0500

    fixed some issues

--- CDM2CSV.py
+++ CDM2CSV.py
@@ -3,26 +3,38 @@ import re
 import sys
 import time
 
+#function uses args as input and returns a list of the collections names in a format like "/sie_meander"
+def getCollectionList():
+	#uses args as info to scrape document.
+	r=requests.get(sys.argv[1], auth=(sys.argv[2], sys.argv[3]));
+	#the data we want is in a drop down box.  This regex matches the content from drop down boxes
+	pattern='value="\/.{1,6}_.{1,16}"'	
+	text=re.findall(pattern,r.text)
+	collection=[]
+
+	#the regex includes 'value="' and '"' in the returned string so take substring to cut those bits off
+	for node in text:
+		collection.append(node[7:-1])
+	#after all data is added to collection
+	return collection
+
+#checks usage
+#TODO: use regex to verify integrity of data
 if len(sys.argv)<4:
-	print("Usage: python3 CDM2CSV.py [collections.carli......exe] [username] [password]")
+	print("Usage: python3 CDM2CSV.py [collections.carli......collections.exe] [username] [password]")
 	sys.exit(1)
-collectionList = getCollectionList(sys.argv)
+
+#EXECUTION BEGIN
+collectionList = getCollectionList()
 idRange = 0, 0 #Tuple to represent the range of ID to be drawn, where idRange[0] is the start ID and idRange[1] is the end ID
 
 for collection in collectionList:
-	for id in idRange[1] - idRange[0]:
-		url = urlTemplate.format(collection, id)
-		print("Processing: " + collection + " | ID: " + id)
-		req = requests.get(url, alloq_redirects=False)
+	print(collection)
+#	for id in idRange[1] - idRange[0]:
+#		url = urlTemplate.format(collection, id)
+#		print("Processing: " + collection + " | ID: " + id)
+#		req = requests.get(url, alloq_redirects=False)
 		#Request Complete
-		time.sleep(0.1)
+#		time.sleep(0.1)
+	
 
-
-def getCollectionList(self):
-	pattern='value="\/.{1,6}_.{1,16}"'
-	r=requests.get(sys.argv[1], auth=(sys.argv[2], sys.argv[3]));
-	text=re.findall(pattern,r.text)
-	collection=[]
-	for node in text:
-		collection.append(node[7:-1])
-	return collection
